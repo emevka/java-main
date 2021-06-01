c01e78e648c2a7aa2a826170f79d8605f2074225
emevka <emevka@yahoo.com>
2021-06-01 18:25:23 +0200

    Edytowanie kontaktu

--- addressbook-web-tests/src/test/java/en/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/en/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,12 +1,17 @@
 package en.stqa.pft.addressbook.tests;
 
+import en.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
-    gotoGroupPage();
-    selectGroup();
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("1", "2", "3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returntoGroupPage();
   }
 }
