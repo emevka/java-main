366757f2636a8c17d95fd70c7d716397c9df811e
emevka <emevka@yahoo.com>
2021-06-10 17:03:49 +0200

    Zrealizowano sprawdzanie warunków wstępnych

--- addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package en.stqa.pft.sandbox.tests;
 
+import en.stqa.pft.sandbox.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData(null, null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returntoGroupPage();
