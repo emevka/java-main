1a59c26e543f2455a47fedbe184cc4fce91edb0b
emevka <emevka@yahoo.com>
2021-06-15 17:01:51 +0200

    Zbiory

--- addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import en.stqa.pft.sandbox.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -20,11 +21,16 @@ public class GroupModificationTests extends TestBase {
 
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("1test", "2test", "3test"));
+    GroupData group = new GroupData(before.get(before.size() -1).getId(),"1test", "2test", "3test");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returntoGroupPage();
 
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
