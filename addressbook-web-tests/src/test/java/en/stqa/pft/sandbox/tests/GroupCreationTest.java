4dde822a7d3f342346e1c95b7f0af2fa5a018a34
emevka <emevka@yahoo.com>
2021-06-17 16:43:24 +0200

    Lekcja 9

--- addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/en/stqa/pft/sandbox/tests/GroupCreationTest.java
@@ -4,6 +4,8 @@ import en.stqa.pft.sandbox.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -14,9 +16,15 @@ public class GroupCreationTest extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
 
-    app.getGroupHelper().createGroup(new GroupData("test1", "header1", "third1"));
+GroupData group = new GroupData("test10", "header1", "third1");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size()+1);
+
+
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
